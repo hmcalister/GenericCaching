f2d7b713de0c2247e48c2b404b3cc007efef294b
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-13 16:34:17 +1200

    Add test for indirect struct parameters

--- tests/cache_test.go
+++ tests/cache_test.go
@@ -125,4 +125,43 @@ func TestCacheDirectInteger(t *testing.T) {
 	}
 }
 
+func TestCacheStructOfPointer(t *testing.T) {
+	type indirectParam struct {
+		A string
+	}
+
+	type params struct {
+		I *indirectParam
+	}
+
+	numCalls := 0
+	var f = func(p params) string {
+		numCalls += 1
+		return p.I.A
+	}
+
+	c := cache.NewCache[params, string](f)
+	p := params{I: &indirectParam{"TEST"}}
+
+	// Call the function many times with repeated parameters
+	for i := 0; i < 10; i += 1 {
+		c.CallWithCache(p)
+	}
+
+	if numCalls != 1 {
+		t.Fatalf("function should have been called once, but was called %v times", numCalls)
+	}
+
+	// Now we change the string on the param and try again
+	// If the cache only cared about the pointer, this will not cause additional calls
+	// and hence the below test will fail
+	p.I.A += " MUTATE"
+	for i := 0; i < 10; i += 1 {
+		c.CallWithCache(p)
+	}
+
+	if numCalls != 2 {
+		t.Fatalf("function should have been called twice, but was called %v times", numCalls)
+	}
+}
 
